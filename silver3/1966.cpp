3d8298c898f2a13a838457d8aebac9c2d5b86be6
joon-lee <naanthonylee@gmail.com>
2023-03-07 21:14:22 +0900

    joon-lee

--- silver3/1966.cpp
+++ silver3/1966.cpp
@@ -4,38 +4,38 @@ using namespace std;
 
 int main(void)
 {
-	int CASE, N, M, cnt = 0;
+	int CASE, N, M, cnt;
 	int	target, tmp_idx, tmp_val, num;
 
 	cin>>CASE;
 	while (CASE--)
 	{
 		target = 0;
-		cnt = 0;
+		cnt = 1;
 		queue< pair <int, int> >q;
-		priority_queue<int>v;
+		priority_queue<int>		v;
+
 		cin>>N>>M;
 		for (int i = 0; i < N; i++)
 		{
 			cin>>num;
 			q.push(make_pair(i, num));
-			v.push_back(num);
+			v.push(num);
 			if (i == M)
 				target = num;
 		}
 		while (q.size() != 0)
 		{
-			if (q.top().first == M)
+			if (q.front().first == M)
 			{
 				if (v.top() == target)
 					break ;
 				q.pop();
-				q.push(target);
-				cnt++;
+				q.push(make_pair(M, target));
 			}
 			else
 			{
-				if (v.top().second() == q.top().second())
+				if (v.top() == q.front().second)
 				{
 					v.pop();
 					q.pop();
@@ -43,14 +43,13 @@ int main(void)
 				}
 				else
 				{
-					tmp_val = q.top().second();
-					tmp_idx = q.top().first();
+					tmp_val = q.front().second;
+					tmp_idx = q.front().first;
 					q.pop();
 					q.push(make_pair(tmp_idx, tmp_val));
-					cnt++;
 				}
 			}
 		}
-		cout<<cnt<<'\n';
+		std::cout<<cnt<<'\n';
 	}
 }
