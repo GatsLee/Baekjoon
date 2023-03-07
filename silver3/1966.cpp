c38deabffd01fa74b9dd7e2d5935527c889441fe
Joon Yeol Lee <joon-lee@c2r8s4.42seoul.kr>
2023-03-07 19:24:35 +0900

    joon-lee

--- silver3/1966.cpp
+++ silver3/1966.cpp
@@ -4,19 +4,53 @@ using namespace std;
 
 int main(void)
 {
-    int CASE , N, M;
-    int num, cnt;
+	int CASE, N, M, cnt = 0;
+	int	target, tmp_idx, tmp_val, num;
 
-    cin>>CASE;
-    while (CASE--)
-    {
-        queue <int>q;
-        cin>>N>>M;
-        for (int i = 0; i < N; i++)
-        {
-            cin>>num;
-            q.push(num);
-        }
-
-    }
+	cin>>CASE;
+	while (CASE--)
+	{
+		target = 0;
+		cnt = 0;
+		queue< pair <int, int> >q;
+		priority_queue<int>v;
+		cin>>N>>M;
+		for (int i = 0; i < N; i++)
+		{
+			cin>>num;
+			q.push(make_pair(i, num));
+			v.push_back(num);
+			if (i == M)
+				target = num;
+		}
+		while (q.size() != 0)
+		{
+			if (q.top().first == M)
+			{
+				if (v.top() == target)
+					break ;
+				q.pop();
+				q.push(target);
+				cnt++;
+			}
+			else
+			{
+				if (v.top().second() == q.top().second())
+				{
+					v.pop();
+					q.pop();
+					cnt++;
+				}
+				else
+				{
+					tmp_val = q.top().second();
+					tmp_idx = q.top().first();
+					q.pop();
+					q.push(make_pair(tmp_idx, tmp_val));
+					cnt++;
+				}
+			}
+		}
+		cout<<cnt<<'\n';
+	}
 }
