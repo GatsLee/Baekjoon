7a10514ecdb72b24bebe479e93159567635aa06b
Joon Yeol Lee <joon-lee@c2r3s2.42seoul.kr>
2023-05-05 15:55:48 +0900

    joon-lee

--- gold4/14502.cpp
+++ gold4/14502.cpp
@@ -5,36 +5,32 @@
 using namespace std;
 
 int N, M;
+int map[8][8];
 queue < pair <int, int> > Q;
-int x_mv[4] = {0,0,-1,1};
-int y_mv[4] = {1,-1,0,0};
+int mv[4][2] = {{0, 1}, {0, -1}, {-1, 0}, {1,0}};
 
-void solve(vector < vector <int> > &map) {
+void solve() {
 	while (!Q.empty()) {
 		int y = Q.front().first;
 		int x = Q.front().second;
-		Q.pop();
-		int cnt = map[y][x];
 		for (int m = 0; m < 4; m++) {
-			int ny = y + y_mv[m];
-			int nx = x + x_mv[m];
+			int ny = y + mv[m][0];
+			int nx = x + mv[m][1];
 			if (ny >= N | ny < 0 | nx >= M | nx < 0)
 				continue ;
 			if (map[ny][nx] == 0) {
 				Q.push(make_pair(ny, nx));
-				map[ny][nx] = cnt + 1;
+				map[ny][nx] = 2;
 			}
 		}
 	}
 }
 
 int main(void) {
-	int num;
 	ios_base::sync_with_stdio(0);
 	cin.tie(0);
 
 	cin>>N>>M;
-	vector < vector <int> > map(N, vector <int>(M));
 	for (int i = 0; i < N; i++) {
 		for (int j = 0; j < M; j++) {
 			cin>>map[i][j];
@@ -42,12 +38,11 @@ int main(void) {
 				Q.push(make_pair(i,j));
 		}
 	}
-	solve(map);
-	cout<<'\n';
-	for (int i = 0; i < N; i++) {
-		for (int j = 0; j < M; j++) {
-			cout<<map[i][j]<<' ';
-		}
-		cout<<'\n';
-	}
+	solve();
+	int cnt = 0;
+	for (int i = 0; i < N; i++)
+		for (int j = 0; j < M; j++)
+			if (map[i][j] == 0)
+				cnt++;
+	cout << cnt << '\n';
 }
